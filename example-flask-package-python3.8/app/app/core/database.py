011e0c73ac6b0c96a7f6d1e43465e63b2564c50b
Pon <pon.m22@gmail.com>
2024-02-15 17:50:01 +0700

    fix to be alpine

--- example-flask-package-python3.8/app/app/core/database.py
+++ example-flask-package-python3.8/app/app/core/database.py
@@ -3,4 +3,6 @@ from ..models.user import User
 users = [
     User("Alice Example", "alice@example.com"),
     User("Bob Example", "bob@example.com"),
+    User("Pon Morin", "pon@gmail.com"),
+    User("Thanabodin Kaewmaha", "jae@gmail.com")
 ]
